0b9e48c1ecf982b3dda19862e3f157d6651270e6
Robert Ponikiewski <dominik.z.2020@gmail.com>
2024-02-16 21:56:55 +0100

    Update AppDelegate.h

--- Filtry1/AppDelegate.h
+++ Filtry1/AppDelegate.h
@@ -1,14 +1,8 @@
-//
-//  AppDelegate.h
-//  Filtry1
-//
-//  Created by MotionVFX on 16/02/2024.
-//
+
 
 #import <Cocoa/Cocoa.h>
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
-
 
 @end
 
