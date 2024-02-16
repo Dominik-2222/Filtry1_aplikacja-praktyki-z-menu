e6a4cfb4715f46fc3f4fe6d5cfa6674085fb39a8
Robert Ponikiewski <dominik.z.2020@gmail.com>
2024-02-16 20:44:37 +0100

    Initial Commit

---
+++ Filtry1/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Filtry1
+//
+//  Created by MotionVFX on 16/02/2024.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
