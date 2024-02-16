f57a76a540252b1df8f678eb92a494016a9726b0
Robert Ponikiewski <dominik.z.2020@gmail.com>
2024-02-16 22:31:32 +0100

    Update AppDelegate.mm

--- Filtry1/AppDelegate.mm
+++ Filtry1/AppDelegate.mm
@@ -6,6 +6,18 @@
 //
 
 #import "AppDelegate.h"
+#include <vector>
+#include <cstdint>
+#include <cmath>
+#include <cstring>
+using namespace std;
+#define RGBA(i) (i).r,(i).g,(i).b,(i).a
+typedef struct pixel_rgb{
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+    uint8_t a;
+}pixel_rgb;
 
 @interface AppDelegate ()
 
@@ -13,6 +25,8 @@
 @end
 
 @implementation AppDelegate
+
+NSString *path=@"/Users/motionvfx/Documents/kwiatek.tiff";
 bool gray_scale_switch=0,native_switch=0;
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
@@ -30,11 +44,48 @@ bool gray_scale_switch=0,native_switch=0;
 
 //
 - (IBAction)image_generator:(id)sender{
+    NSDate *startTime = [NSDate date];//rozpoczecie liczenia czasu
+    NSImage *image =[[NSImage alloc] initWithContentsOfFile:path];
+    NSBitmapImageRep *bitmapRep = [[NSBitmapImageRep alloc] initWithData:[image TIFFRepresentation ]];
+    NSInteger width = [bitmapRep pixelsWide];
+    NSInteger height = [bitmapRep pixelsHigh];
+    NSInteger rowBytes = [bitmapRep bytesPerRow];
+    unsigned char *pixels = [bitmapRep bitmapData];
     
+    
+    for (NSInteger row =  0; row < height; row++) {
+        unsigned char *rowStart = (unsigned char *)(pixels + (row * rowBytes));
+        for (NSInteger col =  0; col < width; col++) {
+            //if(filtr==0){
+                unsigned char grayscale = (*rowStart *  0.3) + (*(rowStart +  1) *  0.59) + (*(rowStart +  2) *  0.11);
+                *rowStart++ = grayscale; // Red
+                *rowStart++ = grayscale; // Green
+                *rowStart++ = grayscale; // Blue
+                rowStart++;
+        //    }
+        //    else if(filtr==1){
+          //      *rowStart++ = (255-*rowStart); // Red
+          //      *rowStart++ = (255-*rowStart); // Green
+          //      *rowStart++ = (255-*rowStart); // Blue
+         //       rowStart++;
+         //   }
+        }
+    }
+    NSDate *endTime = [NSDate date];
+    NSTimeInterval executionTime = [endTime timeIntervalSinceDate:startTime];
+    NSLog(@"Czas: %f", executionTime);
+    NSImage *grayScaleImage = [[NSImage alloc] initWithSize:[bitmapRep size]];
+      [grayScaleImage addRepresentation:bitmapRep];
+    
+   [[self.window contentView]addSubview:imageView];
+    imageView = [[NSImageView alloc]initWithFrame:[self.window.contentView bounds]];
+    [imageView setAutoresizingMask:NSViewWidthSizable | NSViewHeightSizable];
+    [imageView setImage:image];
 }
 
 //obsluga switchow
 - (IBAction)grayScaleSwitch_Changed:(id)sender{
+    
     gray_scale_switch=!gray_scale_switch;
     NSLog(@"zmiana_G: %d",gray_scale_switch);	
 }
